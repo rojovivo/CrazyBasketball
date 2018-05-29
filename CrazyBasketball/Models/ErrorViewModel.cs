c061746dcc6b627a1cf8359fad7bdd508ceb7798
José Francisco Ibarra <josefrancisco.ibarra@gmail.com>
2018-05-29 21:23:42 +0200

    Initial commit

---
+++ CrazyBasketball/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrazyBasketball.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
